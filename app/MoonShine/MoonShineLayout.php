2f2c90502a5e25e0679a77aabd782c85eda818d0
LT <leetoplayaz@yandex.ru>
2023-12-11 12:42:28 +0300

    chore: 2.4

--- app/MoonShine/MoonShineLayout.php
+++ app/MoonShine/MoonShineLayout.php
@@ -5,7 +5,16 @@ declare(strict_types=1);
 namespace App\MoonShine;
 
 use App\MoonShine\Components\DemoVersionComponent;
-use MoonShine\Components\Layout\{Content, Flash, Footer, Header, LayoutBlock, LayoutBuilder, Menu, Sidebar};
+use MoonShine\Components\Layout\{Content,
+    Flash,
+    Footer,
+    Header,
+    LayoutBlock,
+    LayoutBuilder,
+    Menu,
+    Profile,
+    Search,
+    Sidebar};
 use MoonShine\Contracts\MoonShineLayoutContract;
 
 final class MoonShineLayout implements MoonShineLayoutContract
@@ -15,11 +24,14 @@ final class MoonShineLayout implements MoonShineLayoutContract
         return LayoutBuilder::make([
             Sidebar::make([
                 Menu::make()->customAttributes(['class' => 'mt-2']),
+                Profile::make(withBorder: true),
             ]),
             LayoutBlock::make([
                 DemoVersionComponent::make(),
                 Flash::make(),
-                Header::make(),
+                Header::make([
+                    Search::make()
+                ]),
                 Content::make(),
                 Footer::make()->copyright(fn (): string => <<<'HTML'
                         &copy; 2021-2023 Made with ❤️ by
