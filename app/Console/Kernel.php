50bf19044e66bffc14dfe83569090eff1a950ac8
LT <leetoplayaz@yandex.ru>
2024-02-24 19:08:33 +0530

    chore: Upgrade demo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(ResetCommand::class)
+        $schedule->command('app:reset-console')
             ->everyFifteenMinutes();
     }
 
