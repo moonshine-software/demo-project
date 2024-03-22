c54a42b31fae9f46cb5ca8aefef799f26259acd3
Alexander Nikushkin <nikushkin@mail.ru>
2024-03-22 22:13:10 +0500

    Added links to github

--- app/MoonShine/MoonShineLayout.php
+++ app/MoonShine/MoonShineLayout.php
@@ -23,7 +23,7 @@ final class MoonShineLayout implements MoonShineLayoutContract
     {
         return LayoutBuilder::make([
             Sidebar::make([
-                Menu::make()->customAttributes(['class' => 'mt-2']),
+                Menu::make(),
                 Profile::make(withBorder: true),
             ]),
             LayoutBlock::make([
@@ -33,17 +33,24 @@ final class MoonShineLayout implements MoonShineLayoutContract
                     Search::make()
                 ]),
                 Content::make(),
-                Footer::make()->copyright(fn (): string => <<<'HTML'
-                        &copy; 2021-2024 Made with ❤️ by
-                        <a href="https://cutcode.dev"
-                            class="font-semibold text-primary hover:text-secondary"
-                            target="_blank"
-                        >
-                            CutCode
-                        </a>
-                    HTML)->menu([
-                    'https://moonshine-laravel.com' => 'Documentation',
-                ]),
+                Footer::make()
+                    ->copyright(fn(): string => sprintf(
+                        <<<'HTML'
+                            &copy; 2021-%d Made with ❤️ by
+                            <a href="https://cutcode.dev"
+                                class="font-semibold text-primary hover:text-secondary"
+                                target="_blank"
+                            >
+                                CutCode
+                            </a>
+                        HTML,
+                        now()->year
+                    ))
+                    ->menu([
+                        'https://github.com/moonshine-software/moonshine' => 'GitHub',
+                        'https://github.com/moonshine-software/demo-project' => 'Demo project',
+                        'https://moonshine-laravel.com' => 'Documentation',
+                    ]),
             ])->customAttributes(['class' => 'layout-page']),
         ]);
     }
