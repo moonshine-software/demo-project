8e8a7f22d8ba51129615dcaf215ffdb0209e9a96
lee-to <thecutcode@gmail.com>
2026-01-20 18:01:50 +0300

    chore: 4.4.6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
