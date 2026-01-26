dbc99f2b0463353ac029e359768f7cce07b7e996
lee-to <thecutcode@gmail.com>
2026-01-26 17:31:32 +0300

    fix: 500 after reset

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up'
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->authenticateSessions()->redirectTo('/admin/login');
         $middleware->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
